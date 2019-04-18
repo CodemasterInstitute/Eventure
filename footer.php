828a812af7354e375608bdd1b35bf8583930c8b4
Jill Augustus <jma529@gmail.com>
2019-04-18 12:25:19 +0800

    Add php layout

---
+++ footer.php
@@ -0,0 +1,124 @@
+<!-- Footer -->
+<footer class="page-footer font-small stylish-color-dark pt-4">
+
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-md-4 mx-auto">
+
+      <!-- Content -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4">EVENTURE</h5>
+      <p>Make Adventures Possible.</p>
+
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 mx-auto">
+
+      <!-- Links -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Eventure</h5>
+
+      <ul class="list-unstyled">
+        <li>
+          <a href="#!">Home</a>
+        </li>
+        <li>
+          <a href="#!">FAQs</a>
+        </li>
+        <li>
+          <a href="#!">Contact</a>
+        </li>
+      </ul>
+
+    </div>
+    
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 mx-auto">
+
+      <!-- Links -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4">My Account</h5>
+
+      <ul class="list-unstyled">
+        <li>
+          <a href="#!">Sign In</a>
+        </li>
+        <li>
+          <a href="#!">My Dashboard</a>
+        </li>
+        <li>
+          <a href="#!">Create An Event</a>
+        </li>
+
+      </ul>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+<hr>
+
+<!-- Call to action -->
+<ul class="list-unstyled list-inline text-center py-2">
+  <li class="list-inline-item">
+    <h5 class="mb-1">Register for free</h5>
+  </li>
+  <li class="list-inline-item">
+    <a href="#!" class="btn btn-danger btn-rounded">Sign up!</a>
+  </li>
+</ul>
+<!-- Call to action -->
+
+<hr>
+
+<!-- Social buttons -->
+<ul class="list-unstyled list-inline text-center">
+  <li class="list-inline-item">
+    <a class="btn-floating btn-fb mx-1">
+      <i class="fab fa-facebook-f"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-tw mx-1">
+      <i class="fab fa-twitter"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-gplus mx-1">
+      <i class="fab fa-google-plus-g"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-li mx-1">
+      <i class="fab fa-linkedin-in"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-dribbble mx-1">
+      <i class="fab fa-dribbble"> </i>
+    </a>
+  </li>
+</ul>
+<!-- Social buttons -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2019 Copyright: AAA Team
+</div>
+<!-- Copyright -->
+
+</footer>
---
+++ header.php
@@ -0,0 +1,41 @@
+
+
+
+
+<header>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark color-header">
+        <a class="navbar-brand" href="index.html"><i>Eventure</i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Events <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link " href="#">
+                        Members
+                    </a>
+
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#" tabindex="-1">Log In/ Sign Up</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+
+                <button class="btn btn-outline-success my-2 my-sm-0 btn-lg magnify-icon" type="submit"><i
+                        class="fas fa-search"></i></button>
+            </form>
+        </div>
+    </nav>
+</header>
+
+
