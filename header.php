f49553b49fb5220fe87737263ad4578d3f70a039
Jeroen Harpe <43515079+JeroenHarpe@users.noreply.github.com>
2019-04-23 14:51:12 +0800

    Fixed header + CSS

--- header.php
+++ header.php
@@ -1,10 +1,6 @@
-
-
-
-
 <header>
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark color-header">
-        <a class="navbar-brand" href="index.html"><img src="CSS/images/eventure logo 2.3.png" class="logo-icon"></a>
+        <a class="navbar-brand" href="index.php"><img src="CSS/images/eventure logo 2.3.png" class="logo-icon"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,10 +9,20 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item ">
-                    <a class="nav-link" href="#">Events <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="events.php">Events <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="categories.php" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Categories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Free</a>
+                        <a class="dropdown-item" href="#">Music</a>
+                        <a class="dropdown-item" href="#">Comedy</a>
+                        <a class="dropdown-item" href="#">Sport</a>
+                        <a class="dropdown-item" href="#">Food & Wine</a>
+                    </div>
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link " href="#">
@@ -37,5 +43,3 @@
         </div>
     </nav>
 </header>
-
-
