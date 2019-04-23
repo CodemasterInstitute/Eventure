c59573f4a14b17127f04cf8eff487974f0d4ed0b
advenger95 <35837223+advenger95@users.noreply.github.com>
2019-04-23 11:24:19 +0800

    added id tags to member carousel

--- dashboard.php
+++ dashboard.php
@@ -78,7 +78,10 @@
     visibility: visible;
     display: block;
     visibility: visible;
+    
   }
+
+  
 
   .carousel-item {
     margin-right: inherit
@@ -265,11 +268,11 @@
                 </div>
               </div>
             </div>
-            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev" id="carIcon">
+              <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
               <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next" id="carIcon">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
