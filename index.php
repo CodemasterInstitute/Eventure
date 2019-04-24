2d7fcededde4169127470211df97e4a3a781278e
Jeroen Harpe <43515079+JeroenHarpe@users.noreply.github.com>
2019-04-24 12:38:55 +0800

    Fixed NAV search

--- index.php
+++ index.php
@@ -86,8 +86,8 @@ $locationquery = 'SELECT DISTINCT eventAddress FROM events';
         </ul>
         <form class="form-inline my-2 my-lg-0">
 
-          <button class="btn btn-outline-success my-2 my-sm-0 btn-lg magnify-icon" type="submit"><i
-              class="fas fa-search"></i></button>
+          <input id="searchNav" type="text" name="search" placeholder="Start your eventure.."><i id="magnGlass"
+              class="fas fa-search btn"></i>
         </form>
       </div>
     </nav>
