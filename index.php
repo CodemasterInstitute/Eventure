cc664a719bef45935e1e550fe0f937f4779c1abe
advenger95 <35837223+advenger95@users.noreply.github.com>
2019-05-09 16:31:04 +0800

    added new favicon (test)

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ $locationquery = 'SELECT DISTINCT eventCity FROM events';
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
-  <link rel="shortcut icon" href="CSS\images\eventure favcon 1.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="CSS\images\eventure favcon 2.ico" type="image/x-icon">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="CSS/main.css">
