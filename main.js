fc1903249722ca4af19fd3a2f8e8494af598ecec
advenger95 <35837223+advenger95@users.noreply.github.com>
2019-05-13 11:19:22 +0800

    fixed up jquery

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
                 $(document).ready(function() {
+
+// Member Dashboard 'suggested events' carousel slide
           $("#myCarousel").on("slide.bs.carousel", function(e) {
             var $e = $(e.relatedTarget);
             var idx = $e.index();
