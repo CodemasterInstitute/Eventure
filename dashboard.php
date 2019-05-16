d49000ecc7f570c8b63ad280b60cec745ffac27c
advenger95 <35837223+advenger95@users.noreply.github.com>
2019-05-16 15:10:19 +0800

    attempts at fixing dropdown 'personal catagories' button

--- dashboard.php
+++ dashboard.php
@@ -181,8 +181,8 @@ if (isset($_SESSION['username'])) {?>
               <a class="nav-link" href="#"><i class="fas fa-calendar-week"></i> My Events</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-map-pin"></i> Selected Catagories</a>
-              <div class="dropdown-menu">
+              <a class="nav-link dropdown-toggle" href="#pageDropmen" role="button" data-toggle="collapse" aria-haspopup="true" aria-expanded="false"><i class="fas fa-map-pin"></i> Selected Catagories</a>
+              <div class="dropdown-menu" id="pageDropmen">
                 <a class="dropdown-item" href="#">Comedy</a>
                 <a class="dropdown-item" href="#">Music</a>
               </div>
